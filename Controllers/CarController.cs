51d5ea454ffa9122ae02171444f116773a0ac1cf
Pablo <pocuilinn@gmail.com>
2014-04-14 21:14:22 +0100

    first commit

---
+++ Controllers/CarController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarRentals.Models;
+
+namespace CarRentals.Controllers
+{
+    public class CarController : Controller
+    {
+        private CarRentalsEntities db = new CarRentalsEntities();
+
+        // GET: /Car/
+        public ActionResult Index()
+        {
+            var cars = db.Cars.Include(c => c.Company);
+            return View(cars.ToList());
+        }
+
+        // GET: /Car/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // GET: /Car/Create
+        public ActionResult Create()
+        {
+            ViewBag.CompanyID = new SelectList(db.Companies, "CompanyID", "CompanyName");
+            return View();
+        }
+
+        // POST: /Car/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="CarID,CarType,VehicleName,Location,CountryOfMan,Seats,ExColour,InColour,Condition,Available,Convertible,AddInfo,CompanyID")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cars.Add(car);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CompanyID = new SelectList(db.Companies, "CompanyID", "CompanyName", car.CompanyID);
+            return View(car);
+        }
+
+        // GET: /Car/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CompanyID = new SelectList(db.Companies, "CompanyID", "CompanyName", car.CompanyID);
+            return View(car);
+        }
+
+        // POST: /Car/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="CarID,CarType,VehicleName,Location,CountryOfMan,Seats,ExColour,InColour,Condition,Available,Convertible,AddInfo,CompanyID")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CompanyID = new SelectList(db.Companies, "CompanyID", "CompanyName", car.CompanyID);
+            return View(car);
+        }
+
+        // GET: /Car/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: /Car/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Car car = db.Cars.Find(id);
+            db.Cars.Remove(car);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
